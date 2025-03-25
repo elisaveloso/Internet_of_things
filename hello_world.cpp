2f1db85b27c84821a926241b9a1346e587caf339
elisaveloso <elisaaveloso@gmail.com>
2025-03-25 14:10:42 -0300

    first commit

---
+++ hello_world.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+
+
+// Compile the program
+/* To run this code on a Linux terminal, follow these steps:
+
+1. Open a terminal and navigate to the directory containing the file:
+    ```bash
+    cd /home/elisaveloso/Internet_of_things
+    ```
+
+2. Compile the code using `g++`:
+    ```bash
+    g++ -o hello_world hello_world.cpp
+    ```
+
+3. Run the compiled program:
+    ```bash
+    ./hello_world
+    ``` */
